471f4a4ddf29970956a42b6525964ba030cfd420
Igor Majic <majic753@gmail.com>
2022-04-07 20:31:26 +0200

    Started gamemap points

--- client/src/components/map/GameMap.tsx
+++ client/src/components/map/GameMap.tsx
@@ -1,11 +1,39 @@
 import styles from './GameMap.module.scss';
-import MAP_IMG from '../../assets/map.jpg';
+
+const points = [
+  {
+    name: 'Torino',
+    top: '9%',
+    left: '8%',
+  },
+  {
+    name: 'Genova',
+    top: '23.5%',
+    left: '6.5%',
+  },
+  {
+    name: 'Milano',
+    top: '8%',
+    left: '14%',
+  },
+];
 
 export const GameMap = (): JSX.Element => {
   return (
     <div className={styles.Container}>
       <div className={styles.MapContainer}>
-        <img src={MAP_IMG} alt={''} />
+        {points.map((point, index: number) => {
+          return (
+            <div
+              key={index}
+              className={styles.Point}
+              style={{
+                top: point.top,
+                left: point.left,
+              }}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
