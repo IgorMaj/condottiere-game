5d6fea58a1c6a0d8f14ba452c97449624109cb52
majic753@gmail.com <majic753@gmail.com>
2022-07-10 14:01:29 +0200

    Initial algorithm implementation

--- client/src/domain/game-logic/ai/map.ts
+++ client/src/domain/game-logic/ai/map.ts
@@ -3,12 +3,20 @@ import { Bot } from 'boardgame.io/ai';
 import { CreateGameReducer } from 'boardgame.io/internal';
 
 import {
+  BISHOP_CLASS,
   CONDOTTIERE_TOKEN_ID,
+  COURTESAN_CLASS,
+  DRUMMER_CLASS,
+  HEROINE_CLASS,
+  MERCENARY_TYPE,
+  POPE_SCORE_BONUS,
   POPE_TOKEN_ID,
+  SPRING_CLASS,
   TerritoryStatus,
+  WINTER_CLASS,
 } from '../../../utils/constants';
 import { findMaxByAttribute } from '../../../utils/methods';
-import { GameContext, GameState } from '../../entity';
+import { GameContext, GameState, ICardModel, Territory } from '../../entity';
 
 export const MAP_AI = {
   enumerate: (G: GameState, ctx: GameContext) => {
@@ -91,7 +99,156 @@ function generateState(
   };
 }
 
-// TODO Logic depending on state and its type(pope or condottiere token)
+function isPopeState(state: State) {
+  const gameState = state.G as GameState;
+  return (
+    !!gameState.territories.find((t) => t.status === TerritoryStatus.POPE) &&
+    !gameState.territories.find((t) => t.status === TerritoryStatus.BATTLE)
+  );
+}
+
+// Algorithm for score estimation
+// TODO write tests for this
+
+function isEnemyTerritory(
+  territoryName: string,
+  territories: Territory[],
+  playerID: string
+): boolean {
+  return territories.find((t) => t.name === territoryName)?.owner !== playerID;
+}
+
+function isPlayerTerritory(
+  territoryName: string,
+  territories: Territory[],
+  playerID: string
+): boolean {
+  return territories.find((t) => t.name === territoryName)?.owner === playerID;
+}
+
+// for pope states
+// simple - the more enemy territories the token is adjacent to, the better
+// if the territory is adjacent to the player, then it is subject to
+// slight penalization, because of potential blocking
+function popeStateScore(childState: State, playerID: string): number {
+  const territories = (childState.G as GameState).territories;
+  const popeTerritory = territories.find(
+    (t) => t.status === TerritoryStatus.POPE
+  );
+  const adjacentTerritories = popeTerritory?.adjacentTo ?? [];
+  let score = 0;
+  for (let adjacentTerritoryName of adjacentTerritories) {
+    if (isEnemyTerritory(adjacentTerritoryName, territories, playerID)) {
+      score += 5;
+    } else if (
+      isPlayerTerritory(adjacentTerritoryName, territories, playerID)
+    ) {
+      score -= 1;
+    }
+  }
+  console.log('Pope score: ' + score);
+  return score;
+}
+
+function estimateHandStrength(hand: ICardModel[]): number {
+  let strength = 0;
+  let alreadyHasDrummer = false;
+  hand.forEach((card) => {
+    if (card.type === MERCENARY_TYPE) {
+      strength += card.value;
+    } else if (card.class === COURTESAN_CLASS) {
+      strength += 2;
+    } else if (card.class === HEROINE_CLASS) {
+      strength += 10;
+    } else if (card.class === WINTER_CLASS) {
+      strength += hand.find(
+        (c) => c.class === HEROINE_CLASS || c.class === SPRING_CLASS
+      )
+        ? 10
+        : 0;
+    } else if (card.class === SPRING_CLASS) {
+      strength += 6;
+    } else if (card.class === DRUMMER_CLASS) {
+      if (!alreadyHasDrummer) {
+        strength += hand.filter((c) => c.type === MERCENARY_TYPE).length * 2;
+        alreadyHasDrummer = true;
+      }
+    } else if (card.class === BISHOP_CLASS) {
+      strength += 5;
+    }
+  });
+
+  return strength;
+}
+
+// A good hand has greater strength than 50% of all players
+function hasGoodHand(childState: State, playerID: string): boolean {
+  const gameState = childState.G as GameState;
+  const estimatedStrengthsObj: any = {};
+  Object.keys(gameState.players).forEach((key) => {
+    estimatedStrengthsObj[key] = estimateHandStrength(
+      gameState.players[key].hand
+    );
+  });
+
+  const playerEstimatedStength: number = estimatedStrengthsObj[playerID];
+  const estimatedStrengthArr: number[] = Object.values(estimatedStrengthsObj);
+  return (
+    estimatedStrengthArr.filter((elem) => playerEstimatedStength >= elem)
+      .length >
+    estimatedStrengthArr.length / 2
+  );
+}
+
+// for condottiere states:
+// Calculate strength of each hand
+// if the player has a chance of winning (greater strength than most players (> 50%))
+// pick states which places token so it's adjacent to the most player territories
+// if a state also has a token which is adjacent to enemy territories,
+// lower its score for each adjacent enemy territory
+// if the player is unable to beat most opponents(< 50%)  place the token on a territory
+// which is not adjacent to either the player or the enemies
+// (lower the score for each adjacent taken territory)
+
+function condottiereStateScore(
+  childState: State,
+  playerID: string,
+  isGoodHand: boolean
+): number {
+  const territories = (childState.G as GameState).territories;
+  const battleTerritory = territories.find(
+    (t) => t.status === TerritoryStatus.BATTLE
+  );
+  let score = 0;
+  if (isGoodHand) {
+    battleTerritory?.adjacentTo?.forEach((terrName) => {
+      if (isEnemyTerritory(terrName, territories, playerID)) {
+        score -= 1;
+      } else if (isPlayerTerritory(terrName, territories, playerID)) {
+        score += 2;
+      }
+    });
+    return score;
+  } else {
+    battleTerritory?.adjacentTo?.forEach((terrName) => {
+      if (
+        isEnemyTerritory(terrName, territories, playerID) ||
+        isPlayerTerritory(terrName, territories, playerID)
+      ) {
+        score -= 1;
+      }
+    });
+    console.log('Condottiere score: ' + score);
+    return score;
+  }
+}
+
 function generateScore(childState: State, playerID: string): number {
-  return Math.random();
+  const popeState = isPopeState(childState);
+  if (popeState) {
+    return POPE_SCORE_BONUS + popeStateScore(childState, playerID);
+  } else {
+    const goodHand = hasGoodHand(childState, playerID);
+    return condottiereStateScore(childState, playerID, goodHand);
+  }
 }
