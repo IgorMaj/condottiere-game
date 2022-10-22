14893a2eb4485c051a4e48847cb59181db225d85
majic753@gmail.com <majic753@gmail.com>
2022-10-23 00:43:59 +0200

    cors hotfix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
 import { Game } from 'boardgame.io';
-import { Origins, Server } from 'boardgame.io/server';
+import { Server } from 'boardgame.io/server';
 import { MultiplayerGame } from '../../client/src/domain/game-logic/multiplayer/multiplayer-game';
+
+const ALL_ORIGIN = new RegExp('.*');
 
 const server = Server({
   // Provide the definitions for your game(s).
   games: [MultiplayerGame as unknown as Game],
 
-  origins: [
-    // Allow localhost to connect, except when NODE_ENV is 'production'.
-    Origins.LOCALHOST_IN_DEVELOPMENT,
-  ],
+  origins: [ALL_ORIGIN],
 });
 
 server.run(8000, () => console.log('Server is running...'));
