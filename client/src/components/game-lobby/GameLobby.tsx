0edeb46e68dbffbe0512f3ecd9ba1d5e06a46e35
majic753@gmail.com <majic753@gmail.com>
2022-10-23 21:32:36 +0200

    basic lobby styles

--- client/src/components/game-lobby/GameLobby.tsx
+++ client/src/components/game-lobby/GameLobby.tsx
@@ -1,15 +1,20 @@
 import { Lobby } from 'boardgame.io/react';
 import { MultiplayerGame } from '../../domain/game-logic/multiplayer/multiplayer-game';
 import { MultiplayerBoardView } from '../board/Board';
+import './GameLobby.scss';
 
 const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:8000`;
 
 export const GameLobby = (): JSX.Element => {
   return (
-    <Lobby
-      gameServer={SERVER_URL}
-      lobbyServer={SERVER_URL}
-      gameComponents={[{ game: MultiplayerGame, board: MultiplayerBoardView }]}
-    />
+    <div className="game-lobby-container">
+      <Lobby
+        gameServer={SERVER_URL}
+        lobbyServer={SERVER_URL}
+        gameComponents={[
+          { game: MultiplayerGame, board: MultiplayerBoardView },
+        ]}
+      />
+    </div>
   );
 };
