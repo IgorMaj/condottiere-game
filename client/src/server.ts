233a7c9c3c8c6fd0c8453528f51ae6c6345a8f59
majic753@gmail.com <majic753@gmail.com>
2022-11-04 20:14:25 +0100

    Polyfill hotfix

--- client/src/server.ts
+++ client/src/server.ts
@@ -2,6 +2,12 @@ import { Game } from 'boardgame.io';
 import { Server } from 'boardgame.io/server';
 import { MultiplayerGame } from './domain/game-logic/multiplayer/multiplayer-game';
 
+if (typeof String.prototype.replaceAll === 'undefined') {
+  String.prototype.replaceAll = function (match, replace) {
+    return this.replace(new RegExp(match, 'g'), () => replace as string);
+  };
+}
+
 const ALL_ORIGIN = new RegExp('.*');
 const PORT = Number(process.env.PORT) || 8000;
 
